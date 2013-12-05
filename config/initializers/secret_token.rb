c75bc5841edb475d1a58b4a45c45ee5006c71246
Justin Hand <jdhand90@gmail.com>
2013-12-05 14:22:39 -0500

    first Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EveProject::Application.config.secret_key_base = '27220c8a6f34ee32c6c02eb65ae1d98da645bec6cc1bb52da4f44701cc6629d13a35b27080a06a4ad0875f7dbd22f89f9c3e85477b7a5fa414b8fd048ba8e482'
